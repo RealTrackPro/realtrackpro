34a7c19c70b5f19a5235b8ce587be4f6643084ec
RealTrackPro <queenhelen@leveluprealty.org>
2025-05-17 17:45:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
-{
-  "name": "realtrackpro",
-  "version": "1.0.0",
-  "private": true,
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "start": "next start"
-  },
-  "dependencies": {
-    "next": "13.4.12",
-    "react": "18.2.0",
-    "react-dom": "18.2.0"
-  }
-}
+// next.config.js
+module.exports = {
+  reactStrictMode: true
+};
