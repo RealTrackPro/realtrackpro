bdb2d42dfb86e6fb0062f82df9f4e17c1048ba58
RealTrackPro <queenhelen@leveluprealty.org>
2025-05-17 18:20:53 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to RealTrackPro</h1>
+      <p>Your site is live and ready to grow.</p>
+    </div>
+  );
+}
